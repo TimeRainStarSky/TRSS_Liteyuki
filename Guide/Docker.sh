f8a60b77f9eb691feb967c396427971d738eb9cb
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-10-21 23:13:43 +0800

    新增 重装字体

--- Guide/Docker.sh
+++ Guide/Docker.sh
@@ -1,5 +1,5 @@
 #TRSS Liteyuki Docker 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202210140
+NAME=v1.0.0;VERSION=202210210
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B———————————————————————————
 $R TRSS$Y Liteyuki$G Docker$C Script$O
@@ -288,6 +288,6 @@ echo "
 $Y- 正在启动 Docker 容器$O
 "
 docker run -itPd -h TRSS-Liteyuki --name TRSS_Liteyuki -v "$DIR":/root/TRSS_Liteyuki --restart=always trss:liteyuki||abort "Docker 容器启动失败，若要重装容器，请先停止并删除已安装容器"
-echo -n "docker exec -it TRSS_Liteyuki bash /root/TRSS_Liteyuki/Main.sh "'"$@"'>$CMDPATH/$CMD&&chmod 755 $CMDPATH/$CMD||abort "脚本执行命令 $CMDPATH/$CMD 设置失败，手动执行命令：docker exec -it TRSS_Liteyuki bash /root/TRSS_Liteyuki/Main.sh"
+echo -n "docker exec -it TRSS_Liteyuki bash /root/TRSS_Liteyuki/Main.sh "'"$@"'>"$CMDPATH/$CMD"&&chmod 755 "$CMDPATH/$CMD"||abort "脚本执行命令 $CMDPATH/$CMD 设置失败，手动执行命令：docker exec -it TRSS_Liteyuki bash /root/TRSS_Liteyuki/Main.sh"
 echo "
 $G- Docker 容器安装完成，启动命令：$CMD$O"
